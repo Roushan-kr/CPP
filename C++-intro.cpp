bce1ccd43431e14e8c840c79dc5a0faf050a9ffd
roushan <kumar7roushanjha2005@gmail.com>
2023-04-22 16:24:34 +0530

    Added initial information

---
+++ C++-intro.cpp
@@ -0,0 +1,13 @@
+// it just work as a mid level programing concept 
+// a mid level program need programmer knowlage to use memory and dynamic locaton
+// while high level have it own knowlage to do mean it is pre and better define 
+// variable are container to store value
+
+// variable and comment in c++
+# include<iostream>
+using namespace std;
+int main(){
+    // std::cout<<"hello world\n";
+    cout<<"hello world\n";
+    return 0;
+}
